1a89daeb341fe1424a50f1f540e9c38df95b96d2
Dima Ksenchyn <42603078+tokarkin@users.noreply.github.com>
2020-02-08 13:42:11 +0200

    Updates

---
+++ precache-manifest.ba9ded5b55a8208aa1b8e659510c7c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fe94280960b8c2a1ae0fd5e986ebfa",
+    "url": "/Test_films/index.html"
+  },
+  {
+    "revision": "c671cb7693ab5948450c",
+    "url": "/Test_films/static/js/2.fe3e94ed.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Test_films/static/js/2.fe3e94ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08d4bb5a24c9599af1a",
+    "url": "/Test_films/static/js/main.0117b423.chunk.js"
+  },
+  {
+    "revision": "73bc85289b5488b9ec41",
+    "url": "/Test_films/static/js/runtime-main.3096d874.js"
+  },
+  {
+    "revision": "ce4b4809de3bb0e3fc98d56e564f37a3",
+    "url": "/Test_films/static/media/358477.700xp.ce4b4809.jpg"
+  }
+]);
